7d23d09de6bd678aba21f8d3f0d904a70e73f66e
fschaff <groovetime111@gmail.com>
2017-10-27 18:49:15 +0100

    various fixes and documentation updates

--- R/browse.R
+++ R/browse.R
@@ -4,32 +4,31 @@
 #' Browser shows variable details such as names, labels, range, distinct values, class, typeof, non-missing cases and missing cases.
 #' @param x The dataset to be opened in the Viewer.
 #' @keywords browse browser variable variables
-#' @importFrom Hmisc label
 #' @importFrom stats median na.omit sd var
 #' @import utils
 #' @examples
 #' # create data.frame
 #' numeric <- c(1, 2, 2, 3, 3, 3, 4, 4, 4, 4)
 #' labelled.numeric <- c(1, 2, 2, 3, 3, 3, 4, 4, 4, 4)
-#' attr(labelled.numeric, "label") <- "my.variable.label"
+#' attributes(labelled.numeric)$label <- "my.variable.label"
 #' df <- data.frame(numeric, labelled.numeric)
-#'
-#' # open data.frame in Viewer window
+#' # View(df)
 
 #' @export
 browse <- function(x) {
   variable.name <- names(x)
-  if (inherits(x, "labelled"))
-    variable.label <- Hmisc::label(x)
-  else if (!inherits(x, "labelled"))
-    variable.label <- cbind(lapply(x, function(y) {c("-")}))
-  distinct.values <- cbind(lapply(x, function(y) {length(table(y))} )) # shows number of distinct values without NA
+  variable.label <- cbind(lapply(x,
+                                 function(y) {
+                                   if (is.null(attributes(y))) { c("-") }
+                                   else if (!is.null(attributes(y)$label)) { as.character(attributes(y)$label) }
+                                 }))
+  distinct.values <- cbind(lapply(x, function(y) {length(table(y))} ))
   range <- cbind(lapply(x,
                         function(y) {
                           if (is.character(y)) { c("-") }
                           else if (is.factor(y)) { c("-") }
                           else if (is.numeric(y)) { range(y, na.rm = TRUE, finite = TRUE)}
-                          else c("Something went wrong.")
+                          else c(NA_character_)
                         }))
   N <- colSums(!is.na(x))
   missings <- colSums(is.na(x))
@@ -37,8 +36,7 @@ browse <- function(x) {
   typeof <- cbind(lapply(x, function(y) {typeof(y)} ))
   data.frame <- data.frame(variable.name, variable.label, range, distinct.values, class, typeof, N, missings)
   row.names(data.frame) <- NULL
-  assign(paste0("browse.", deparse(substitute(x))), data.frame, envir = .GlobalEnv)
-  message("The data was saved as '", paste0("browse.", deparse(substitute(x))), "' in the global environment. Open it in the Viewer with View(", paste0("browse.", deparse(substitute(x)), ")."))
+  return(data.frame)
   }
 
 
